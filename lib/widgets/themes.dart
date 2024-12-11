5123f611c8272fbf9273437e1da25904091435bf
rajvardhan1 <raj.vardhan305844@gmail.com>
2024-12-11 09:06:19 +0530

    adding price and button

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   static ThemeData get lightTheme => ThemeData(
         primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
+        fontFamily: GoogleFonts.poppins().fontFamily,
         appBarTheme: AppBarTheme(
           color: Colors.white,
           elevation: 0.0,
@@ -13,7 +13,11 @@ class MyTheme {
           titleTextStyle: GoogleFonts.latoTextTheme().titleLarge,
         ),
       );
-      static ThemeData darkTheme(BuildContext context) => ThemeData(
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
         brightness: Brightness.dark,
       );
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkbluishColor = Color(0xff403b58);
+
+  static var darkBluishColor;
 }
