1756b80e5b6818b1d15d6358214155a454779cfb
shoyeb <shoyebmalik849@gmail.com>
2024-12-14 14:55:22 +0530

    create new folder Raj

---
+++ lib/raj/raj.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class Raj extends StatelessWidget {
+  const Raj({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('RAJ BAr'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
