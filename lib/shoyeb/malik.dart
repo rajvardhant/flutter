b988d1894e8abce79c6b960d7dcf0c2eba329395
shoyeb <shoyebmalik849@gmail.com>
2024-12-15 12:27:13 +0530

    malik file added

---
+++ lib/shoyeb/malik.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class Malik extends StatelessWidget {
+  const Malik({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Malik'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
